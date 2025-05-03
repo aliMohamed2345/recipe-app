14e2e3b67e008f4de8f4d8362c4e9b1c8190145d
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-05-03 08:16:50 +0300

    finishing the nav design

--- src/app/Components/Nav/Nav.tsx
+++ src/app/Components/Nav/Nav.tsx
@@ -1,19 +1,22 @@
-// import SideMenu from "@/app/Components/Nav/SideMenu";
+"use client";
 // import SearchArea from "@/app/Components/Nav/SearchArea";
+import SideMenu from "./SideMenu";
 import Theme from "@/app/Components/Nav/Theme";
 import Link from "next/link";
 import Search from "./Search";
+import { usePathname } from "next/navigation";
 const Nav = () => {
+  const path = usePathname();
   return (
-    <header className="border-b-1 border-border bg-background  fixed w-full z-10 ">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 bg-background">
+    <header className="border-b-1 border-border fixed w-full z-10 ">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
         <div className="flex h-16 items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
             <div className="flex items-center gap-3 ">
-              {/* <SideMenu /> */}
+              <SideMenu />
               <Link
                 href="/"
-                className="text-3xl font-bold hover:text-text_green  transition "
+                className="text-xl sm:text-3xl font-bold hover:text-green transition "
               >
                 CookBooker
               </Link>
@@ -21,17 +24,45 @@ const Nav = () => {
           </div>
 
           <div className="hidden md:block font-bold">
-            <ul className="flex items-center gap-5">
-              <li className="hover:bg-accent p-2 border-md ">Categories</li>
-              <li className="hover:bg-accent p-2 border-md ">Nutrition</li>
-              <li className="hover:bg-accent p-2 border-md ">Discover</li>
-              <li className="hover:bg-accent p-2 border-md ">Favorites</li>
-            </ul>
+            <div className="flex items-center gap-5">
+              <Link
+                href={"/category"}
+                className={`hover:bg-accent p-2 transition rounded-md ${
+                  path.includes(`category`) && `text-green`
+                } `}
+              >
+                Categories
+              </Link>
+              <Link
+                href={"/nutrition"}
+                className={`hover:bg-accent p-2 transition rounded-md ${
+                  path.includes(`nutrition`) && `text-green`
+                }`}
+              >
+                Nutrition
+              </Link>
+              <Link
+                href={`/recipes`}
+                className={`hover:bg-accent p-2 transition rounded-md ${
+                  path.includes(`recipes`) && `text-green`
+                }`}
+              >
+                Discover
+              </Link>
+              <Link
+                href={`/favorites`}
+                className={`hover:bg-accent p-2 transition rounded-md ${
+                  path.includes(`favorites`) && `text-green`
+                }`}
+              >
+                Favorites
+              </Link>
+            </div>
           </div>
 
           <div className="gap-3 cursor-pointer flex items-center ">
-            <Theme />
             <Search />
+            <Theme />
           </div>
         </div>
       </div>
--- src/app/Components/Nav/Search.tsx
+++ src/app/Components/Nav/Search.tsx
@@ -3,14 +3,13 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { FaClockRotateLeft } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
-import { IoArrowBackSharp, IoImage, IoVideocam } from "react-icons/io5";
+import { IoArrowBackSharp } from "react-icons/io5";
 import { useRouter } from "next/navigation";
 import { IoClose } from "react-icons/io5";
 const Search = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   const [openSearch, setOpenSearch] = useState<boolean>(false);
   const [searchValues, setSearchValues] = useState<string[]>([]);
-  const [mediaOption, setMediaOption] = useState<string>("photo");
   const router = useRouter();
   useEffect(() => {
     // Load stored search values from localStorage
@@ -37,7 +36,7 @@ const Search = () => {
       localStorage.setItem("search-values", JSON.stringify(updatedValues)); // Update localStorage
       setSearchValue("");
 
-      router.push(`/search/${mediaOption}s/${searchValue}?q=${searchValue}`);
+      router.push(`/search/${searchValue}`);
     } else {
       e.preventDefault();
     }
@@ -60,7 +59,7 @@ const Search = () => {
       />
 
       {openSearch && (
-        <div className={` bg-primary w-full h-[850px] inset-0 absolute `}>
+        <div className={` bg-secondary w-full h-screen inset-0 absolute`}>
           <div className="flex items-center gap-2 mt-2">
             <button
               onClick={() => setOpenSearch(false)}
@@ -80,30 +79,6 @@ const Search = () => {
             />
           </div>
           <div className="h-px w-full bg-gray-400 relative my-4"></div>
-          <div className="w-[80%] rounded-xl bg-slate-50 mx-auto my-4 relative border-2 border-slate-300 dark:border-gray-700 flex items-center transition-all">
-            <button
-              type="button"
-              className={`flex text-gray-700 items-center rounded-l-lg hover:bg-amber-500 justify-center gap-2 p-1 w-1/2 ${
-                mediaOption === "photo" && "bg-amber-400"
-              } transition-all hover:opacity-100`}
-              onClick={() => setMediaOption("photo")}
-            >
-              <IoImage />
-              <span>photo</span>
-            </button>
-            <div className="h-full bg-slate-400 w-0.5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-            <button
-              type="button"
-              className={`flex text-gray-700 items-center rounded-r-lg hover:bg-amber-500 justify-center gap-2 p-1 w-1/2 transition-all hover:opacity-100 ${
-                mediaOption === "video" && "bg-amber-400"
-              }`}
-              onClick={() => setMediaOption("video")}
-            >
-              <IoVideocam />
-              <span>video</span>
-            </button>
-          </div>
-          <div className="h-px w-full bg-gray-400 relative my-4"></div>
           <div className="mx-3">
             {searchValues
               .slice(-4)
@@ -112,7 +87,7 @@ const Search = () => {
                 <Link
                   key={i}
                   href={{
-                    pathname: `/search/${mediaOption}s/${value}`,
+                    pathname: `/search/${value}`,
                     query: { q: value },
                   }}
                   className="p-3 rounded-md transition-all text-text_color"
