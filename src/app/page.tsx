0e74fb74782eac1dec0e68dda53852c877554c14
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-05-03 19:18:26 +0300

    finishing the nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useFetch } from "./hooks/useFetch";
+
+interface recipeType {
+  results: [
+    {
+      id: number;
+      title: string;
+      image: string;
+      imageType: string;
+    }
+  ];
+}
+
 export default function Home() {
+  const query = `chicken`;
+  const { data } = useFetch<recipeType>({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/recipes/complexSearch?query=${query}&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&fillIngredients=true`,
+  });
+  console.log(data);
   return <div></div>;
 }
